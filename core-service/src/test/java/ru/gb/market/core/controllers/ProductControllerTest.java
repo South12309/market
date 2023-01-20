d04a22c8263858931556e97690011ea7985f3225
South12309 <south123@mail.ru>
2023-01-20 20:56:37 +0300

    doing Test ProductController

--- core-service/src/test/java/ru/gb/market/core/controllers/ProductControllerTest.java
+++ core-service/src/test/java/ru/gb/market/core/controllers/ProductControllerTest.java
@@ -1,26 +1,47 @@
 package ru.gb.market.core.controllers;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import ru.gb.market.api.ProductDto;
 import ru.gb.market.core.CoreServiceApplicationTests;
 import ru.gb.market.core.converters.ProductConverter;
 import ru.gb.market.core.services.ProductService;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
+
 class ProductControllerTest extends CoreServiceApplicationTests {
     @Autowired
-    private ProductService productService;
+    ProductService productService;
     @Autowired
-    private ProductConverter productConverter;
+    ProductConverter productConverter;
+    @Autowired
+    WebTestClient webTestClient;
+
     @BeforeEach
     void setUp() {
+
     }
 
     @Test
     void getAllProducts() {
+        Page<ProductDto> products = webTestClient.get()
+                .uri("/api/v1/products")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(PageImpl.class)
+                .returnResult()
+                .getResponseBody();
+
+        Assertions.assertNotNull(products.getContent());
+
     }
 
     @Test
--- core-service/src/test/java/ru/gb/market/core/services/ProductServiceTest.java
+++ core-service/src/test/java/ru/gb/market/core/services/ProductServiceTest.java
@@ -1,13 +1,37 @@
 package ru.gb.market.core.services;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.gb.market.api.ProductDto;
+import ru.gb.market.api.ResourceNotFoundException;
 import ru.gb.market.core.CoreServiceApplicationTests;
+import ru.gb.market.core.entities.Product;
+import ru.gb.market.core.repositories.ProductRepository;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductServiceTest extends CoreServiceApplicationTests {
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    CategoryService categoryService;
+    @Autowired
+            ProductService productService;
+
+    String title="productTitle";
+    BigDecimal price=new BigDecimal(100);
+
+    @BeforeEach
+    void prepare() {
+        ProductDto productDto= new ProductDto(1L,title, price, "Food");
+    }
 
     @Test
+
     void findAll() {
     }
 
@@ -17,6 +41,12 @@ class ProductServiceTest extends CoreServiceApplicationTests {
 
     @Test
     void createNewProduct() {
+
+        Product product = new Product();
+        product.setTitle(title);
+        product.setPrice(price);
+      //  product.setCategory(categoryService.findByTitle(productDto.getCategoryTitle()).orElseThrow(() -> new ResourceNotFoundException("Категория с названием: " + productDto.getCategoryTitle() + " не найдена")));
+        productRepository.save(product);
     }
 
     @Test

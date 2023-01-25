36f750c5235b453cf9358610f4596feb865f9102
South12309 <south123@mail.ru>
2023-01-25 10:38:32 +0300

    core-service controllers, services and authController done

---
+++ auth-service/src/test/java/ru/gb/market/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.market.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+public abstract class AuthServiceApplicationTests {
+
+}
